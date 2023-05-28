9484cee548225dd29a2858506e36dab091489ed1
Daniel <110098882+deshchk@users.noreply.github.com>
2023-05-28 23:07:34 +0200

    Add files via upload
    update with new features

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 document.addEventListener('alpine:init', () => {
   Alpine.data('data', () => ({
 
+    limiter: true,
+    top: true,
+
     cardClick(el) {
       el.style.transform = 'none'; el.style.boxShadow = 'none'
     },
